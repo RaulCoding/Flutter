dfe9427b8262028e0bc6b260e72d0ccceeac5657
shistastic <adamjamesrowe@outlook.es>
2023-05-01 12:48:51 +0200

    Corrected Dark SDK version Non-DEV && added VideoPost cast to map

--- toktik/lib/presentation/providers/discover_provider.dart
+++ toktik/lib/presentation/providers/discover_provider.dart
@@ -1,31 +1,23 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/infrastructure/models/local_video_model.dart';
 import 'package:toktik/shared/data/local_video_post.dart';
 
-class DiscoverProvider extends ChangeNotifier{
-  
+class DiscoverProvider extends ChangeNotifier {
   //TODO: Repository, DataSource
-  
-  bool initialLoading =true;
+
+  bool initialLoading = true;
   List<VideoPost> videos = [];
-  
-  
+
   Future<void> loadNextPage() async {
-    
-    await Future.delayed( const Duration( seconds: 2));
-    
-    final List<VideoPost> newVideos = videoPosts.map(
-      (video) => LocalVideoModel.fromJson(video).toVideoPostEntity()
-    ).toList();
-    
-    
+    await Future.delayed(const Duration(seconds: 2));
+
+    final List<VideoPost> newVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+        .toList()
+        .cast<VideoPost>();
+
     videos.addAll(newVideos);
     notifyListeners();
-    
   }
-    
-  
 }
