0defebe4d40d9fb02e3de0578f6940dbdb265041
Raul <raulcodelearning@gmail.com>
2023-05-05 17:44:46 +0200

    rutas con nombre implementado

--- wigets_app/lib/presentation/screens/home/home_screen.dart
+++ wigets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:wigets_app/config/menu/menu_items.dart';
+// import 'package:wigets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  
+  static const String name = 'home_screen';
+  
   const HomeScreen({super.key});
 
   @override
@@ -58,7 +62,10 @@ class _CustomLisTile extends StatelessWidget {
         //     ),
         //   );
         //Navigator.pushNamed(context, menuItem.link);
-      context.push(menuItem.link);
+        // context.pushNamed(CardsScreen.name);
+        context.push(menuItem.link);
+        
+        
       },
     );
   }
