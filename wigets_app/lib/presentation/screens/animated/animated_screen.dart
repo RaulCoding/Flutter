fc5fc44142b7edc077eb7f5b8a761b44b318fdba
Raul <raulcodelearning@gmail.com>
2023-05-06 09:42:19 +0200

    screens creadas y barril actualizado

---
+++ wigets_app/lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AnimnatedScreen extends StatelessWidget {
+  
+  static const name ='animated_screen';
+  
+  const AnimnatedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
