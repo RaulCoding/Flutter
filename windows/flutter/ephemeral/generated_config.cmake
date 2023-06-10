db032d2bc70fd0dfd51fa3713c8798d3e40987d8
Raúl Viloria <raulcodelearning@gmail.com>
2023-06-10 15:59:15 +0200

    Mari Carmen

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Documentos\\Flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\raulv\\Desktop\\Flutter\\cinemapedia" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Documentos\\Flutter"
+  "PROJECT_DIR=C:\\Users\\raulv\\Desktop\\Flutter\\cinemapedia"
+  "FLUTTER_ROOT=D:\\Documentos\\Flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\raulv\\Desktop\\Flutter\\cinemapedia\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\raulv\\Desktop\\Flutter\\cinemapedia"
+  "FLUTTER_TARGET=C:\\Users\\raulv\\Desktop\\Flutter\\cinemapedia\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yYTM0MDFjOWJiYjVhOWE5YWVjNzRkNGY3MzVkMThhOWRkM2ViZjJkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\raulv\\Desktop\\Flutter\\cinemapedia\\.dart_tool\\package_config.json"
+)
